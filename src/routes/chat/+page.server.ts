2bcd375e6b811d62bfdce0819979d69815e337b2
axel-rock <rockaxel@gmail.com>
2026-05-07 17:31:17 +0200

    feat: working chat

--- src/routes/chat/+page.server.ts
+++ src/routes/chat/+page.server.ts
@@ -6,10 +6,15 @@ export const actions: Actions = {
 	default: async ({ request }) => {
 		const data = await request.formData();
 		const message = data.get('message');
+		let messages = data.get('messages');
+		console.log({messages, message});
+		const parsedMessages = JSON.parse(messages as string);
 
 		if (!message) {
 			return fail(400, { missing: true });
 		}
+
+		parsedMessages.push({ role: 'user', content: message });
 
 		const response = await fetch('https://api.openai.com/v1/chat/completions', {
 			method: 'POST',
@@ -19,12 +24,17 @@ export const actions: Actions = {
 			},
 			body: JSON.stringify({
 				model: 'gpt-5.4-mini',
-				messages: [{ role: 'user', content: message }],
+				messages: parsedMessages,
 				temperature: 0
 			})
 		});
 
 		const result = await response.json();
+
+		if (!response.ok || !result.choices?.length) {
+			return fail(500, { error: result.error?.message ?? 'Unknown API error' });
+		}
+
 		const reply = result.choices[0].message.content;
 		return { reply };
 	}
--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -4,22 +4,55 @@
 	import { enhance } from '$app/forms';
 	import { onMount } from 'svelte';
 
-	let messages = [];
+	let messages = [
+    {
+      role: 'system',
+      content: 'You are a helpful assistant that can answer questions and help with tasks.'
+    }
+  ];
+
 
 	onMount(() => {
+    messages = JSON.parse(localStorage.getItem('messages') ?? '[]');
 	});
 </script>
 
 <h1>Chat</h1>
 
 <form method="POST" use:enhance={({ formData }) => {
+  // When the use sends a message
+  console.log({formData});
+  const newUserMessage = formData.get('message');
+  console.log({newUserMessage});
 
-	return async ({ result }) => {
+  messages.push({role: 'user', content: newUserMessage});
+
+  localStorage.setItem('messages', JSON.stringify(messages));
+
+  messages = messages
+	
+  return async ({ result }) => {
+    // When the LLM is done
+    console.log({result});
+
+    messages.push({role: 'assistant', content: result.data.reply});
+
+    localStorage.setItem('messages', JSON.stringify(messages));
+    
+    messages = messages
 	};
 }}>
 	<label>
 		Message
 		<input name="message" type="text" required minlength="1" autocomplete="off" />
+    <input type="hidden" name="messages" value={JSON.stringify(messages)} />
 	</label>
 	<button>Send</button>
 </form>
+
+<pre>{JSON.stringify(messages, null, 2)}</pre>
+
+<button onclick={() => {
+  localStorage.removeItem('messages');
+  messages = [];
+}}>Clear</button>
